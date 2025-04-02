e885f9b2b6e56f4f77634051702afb87af06d1fe
wilyJ80 <abjurandam@gmail.com>
2025-04-02 09:07:06 -0300

    ajustar r2 p2

---
+++ src/main/java/SOLIDroteiros/roteiro2/parte2/PedidoService.java
@@ -0,0 +1,34 @@
+package SOLIDroteiros.roteiro2.parte2;
+
+import java.util.List;
+
+public class PedidoService {
+	private List<String> itens;
+	private double total;
+	private RegraDesconto regraDesconto;
+
+	public PedidoService(List<String> itens, RegraDesconto regraDesconto) {
+		this.itens = itens;
+		this.regraDesconto = regraDesconto;
+		this.total = calcularTotal();
+	}
+
+	private double calcularTotal() {
+		double total = 0;
+		for (String item : itens) {
+			if (item.equals("Camiseta")) {
+				total += 50.0;
+			} else if (item.equals("Calca")) {
+				total += 100.0;
+			} else if (item.equals("Jaqueta")) {
+				total += 200.0;
+			}
+		}
+		return total;
+	}
+
+	public void processarPedido(double desconto) {
+		double valorFinal = regraDesconto.calcular(this.total);
+		System.out.println("Pedido processado. Valor final com desconto: R$" + valorFinal);
+	}
+}
---
+++ src/main/java/SOLIDroteiros/roteiro2/parte2/TestePedido.java
@@ -0,0 +1,12 @@
+package SOLIDroteiros.roteiro2.parte2;
+
+import java.util.Arrays;
+
+public class TestePedido {
+	public static void main(String[] args) {
+		PedidoService pedido = new PedidoService(Arrays.asList("Camiseta", "Calca", "Jaqueta"));
+		pedido.processarPedido(0.1);
+		pedido.processarPedido(0.0);
+		pedido.processarPedido(0.4);
+	}
+}
