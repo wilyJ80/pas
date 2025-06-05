992005e702bc830d9795b42aac4d30c6d86c73d2
wilyJ80 <abjurandam@gmail.com>
2025-06-05 17:38:36 -0300

    feat: finish r6 p2

---
+++ src/main/java/PADROESroteiros/roteiro6/parte1/TesteSemFacade.java
@@ -0,0 +1,17 @@
+package PADROESroteiros.roteiro6.parte1;
+
+public class TesteSemFacade {
+	public static void main(String[] args) {
+		Database db = new Database();
+		Cliente cli01 = new Cliente(1, "Jose");
+		db.addCliente(cli01);
+		CarrinhoCompra carrinho = new CarrinhoCompra();
+		cli01.setCarrinho(carrinho);
+		Produto p1 = db.selectProduto(1);
+		Produto p2 = db.selectProduto(2);
+		cli01.getCarrinho().addProduto(p1);
+		cli01.getCarrinho().addProduto(p2);
+		double total = cli01.getCarrinho().getTotalCompra();
+		db.processarPagamento(cli01, total);
+	}
+}
