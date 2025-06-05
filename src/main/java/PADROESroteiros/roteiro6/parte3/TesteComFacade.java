711ce21e74a98cc01c4a68de8305f97532d45bf0
wilyJ80 <abjurandam@gmail.com>
2025-06-05 17:45:20 -0300

    feat: finish r6 p3

---
+++ src/main/java/PADROESroteiros/roteiro6/parte3/TesteComFacade.java
@@ -0,0 +1,11 @@
+package PADROESroteiros.roteiro6.parte3;
+
+public class TesteComFacade {
+	public static void main(String[] args) {
+		Facade facade = new Facade();
+		facade.registrarCliente("Jose", 222);
+		facade.comprar(1, 222);
+		facade.comprar(2, 222);
+		facade.finalizarCompra(222);
+	}
+}
