641ca944a96cb5bf0cc7147b428c4f1803fd92f8
wilyJ80 <abjurandam@gmail.com>
2025-06-07 09:50:04 -0300

    finish padroes r7 p1, p2

---
+++ src/main/java/PADROESroteiros/roteiro7/parte2/TesteBoleto.java
@@ -0,0 +1,15 @@
+package PADROESroteiros.roteiro7.parte2;
+
+public class TesteBoleto {
+	public static void main(String[] args) {
+		BancoCaixa banco = new BancoCaixa();
+		try {
+			banco.gerarBoleto(10, 100);
+			banco.gerarBoleto(30, 100);
+			banco.gerarBoleto(60, 100);
+			banco.gerarBoleto(90, 100);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
